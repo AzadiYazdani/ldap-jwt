4020bd2f9ad8ab076ad12cee054b0e7e953752cc
A.Yazdani <yaz.azadeh@gmail.com>
2023-07-04 16:39:08 +0330

    Add dependencies of jpa and jdbc

--- src/main/java/com/thunder/light/DemoApplication.java
+++ src/main/java/com/thunder/light/DemoApplication.java
@@ -2,8 +2,12 @@ package com.thunder.light;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="com.thunder.light")
+@EnableJpaRepositories("com.thunder.light")
+@EntityScan("com.thunder.light.database.entity")
 public class DemoApplication {
 
     public static void main(String[] args) {
